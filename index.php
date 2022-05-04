6d3c76a80c70ce00f241a89fa76b4af7a09acf89
TheresaRoukaibe <102797454+TheresaRoukaibe@users.noreply.github.com>
2022-05-04 23:53:32 +0300

    home page

---
+++ index.php
@@ -0,0 +1,287 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title> Discover Lebanon Your Way! </title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+	
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-light fixed-top" id="mainNav">
+            <div class="container">
+			 <a class="navbar-brand" href="#page-top"><img src="assets/img/flag.jpg" width = '100' height = '100' alt="..." /></a>
+                <a class="navbar-light" href="#page-top"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars ms-1"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                        <li class="navbar navbar-dark bg-success"><a class="nav-link" href="#services">Services</a></li>
+                        <li class="navbar navbar-dark bg-success"><a class="nav-link" href="#about">Overview</a></li>
+                        <li class="navbar navbar-dark bg-success"><a class="nav-link" href="#team">Sign Up</a></li>
+                        <li class="navbar navbar-dark bg-success"><a class="nav-link" href="#contact">Sign In</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container">
+                <div class="masthead-subheading">Welcome To Lebanon!</div>
+                <div class="masthead-heading text-uppercase">It's Nice To See You</div>
+                <a class="btn btn-danger btn-xl text-uppercase" href="#services">Go on a Website Tour first !</a>
+            </div>
+        </header>
+        <!-- Services-->
+        <section class="page-section" id="services">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Services</h2>
+                    <h3 class="section-subheading text-muted">What you'll find if you keep scrolling</h3>
+                </div>
+                <div class="row text-center">
+                    <div class="col-md-4">
+                        <span class="fa-stack fa-4x">
+                            <i class="fas fa-circle fa-stack-2x text-danger"></i>
+                            <i class="fas fa-archway fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <h4 class="my-3">Sites to Visit</h4>
+                        <p class="text-muted">Discover new places near you and connect with undiscovered gems and the beauty of Lebanon! </p>
+                    </div>
+                    <div class="col-md-4">
+                        <span class="fa-stack fa-4x">
+                            <i class="fas fa-circle fa-stack-2x text-danger"></i>
+                            <i class="fas fa-city fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <h4 class="my-3">Places to Try</h4>
+                        <p class="text-muted"> Benefit local businesses and browse through restaurants, amusement parks, outdoor activities available.  </p>
+                    </div>
+                    <div class="col-md-4">
+                        <span class="fa-stack fa-4x">
+                            <i class="fas fa-circle fa-stack-2x text-danger"></i>
+                            <i class="fas fa-child fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <h4 class="my-3">Customized Recommendations</h4>
+                        <p class="text-muted">No more "What should we do?" and endless hours to decide. With our help receive a customized recommendation based on your desired budget, area, number of people and even hobbies ! </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+     
+        <!-- About-->
+        <section class="page-section" id="about">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Activities Overview</h2>
+                    <h3 class="section-subheading text-muted">What you'll discover</h3>
+                </div>
+                <ul class="timeline">
+                    <li>
+                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/1.jpg" alt="..." /></div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>Outdoor Activities</h4>
+          
+                            </div>
+                            <div class="timeline-body"><p class="text-muted">Discover places to climb, surf, ski, swim, hike, camp, go kayaking...</p></div>
+                        </div>
+                    </li>
+                    <li class="timeline-inverted">
+                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/2.jpg" alt="..." /></div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>Indoor Activities</h4>
+                               
+                            </div>
+                            <div class="timeline-body"><p class="text-muted">Discover places to go bowling, party, escape rooms, laser games and places to go when you're not feeling like being outdoors. </p></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/3.jpg" alt="..." /></div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>Restaurants</h4>
+         
+                            </div>
+                            <div class="timeline-body"><p class="text-muted"> When hungry, discover a variety of cuisines based on your preferences. </p></div>
+                        </div>
+                    </li>
+          
+                    <li class="timeline-inverted" >
+                        <div class="timeline-image" style = "background-color:#FF0000">
+                            <h4>
+                                And
+                                <br />
+                                Many
+                                <br />
+                                More...
+                            </h4>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
+ 
+		 <section class="page-section" id="team">
+            <div class="container">
+			
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Sign Up and begin Discovering !</h2>
+                </div>
+                <!-- * * * * * * * * * * * * * * *-->
+                <!-- * * SB Forms Contact Form * *-->
+                <!-- * * * * * * * * * * * * * * *-->
+                <!-- This form is pre-integrated with SB Forms.-->
+                <!-- To make this form functional, sign up at-->
+                <!-- https://startbootstrap.com/solution/contact-forms-->
+                <!-- to get an API token!-->
+                <form id="contactForm" data-sb-form-api-token="API_TOKEN" action="signUp.php">
+		
+                    <div class="row align-items-stretch mb-5">
+	
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <!-- First Name input-->
+                                <input class="form-control" id="user_firstname" name = "user_firstname" type="text" placeholder="First Name *" data-sb-validations="required"/>
+                                <div class="invalid-feedback" data-sb-feedback="user_firstname:required">A first name is required.</div>
+                            </div>
+							<div class="form-group">
+                                <!-- First Name input-->
+                                <input class="form-control" id="user_lastname" name="user_lastname" type="text" placeholder="Last Name *" data-sb-validations="required" />
+                                <div class="invalid-feedback" data-sb-feedback="user_lastname:required">A Last name is required.</div>
+                            </div>
+                            <div class="form-group">
+                                <!-- Gender input-->
+                                <input class="form-control" id="user_gender" name ="user_gender" type="text" placeholder="Gender *" data-sb-validations="required" />
+                                <div class="invalid-feedback" data-sb-feedback="user_gender:required">Please enter a gender.</div>
+                     
+                            </div>
+                            <div class="form-group mb-md-0">
+                                <!-- Age input-->
+                                <input class="form-control" id="user_age" name="user_age" type="number" placeholder="Age *" data-sb-validations="required" />
+                                <div class="invalid-feedback" data-sb-feedback="user_age:required">Age is required.</div>
+                            </div>
+							<div 
+							 <label class="form-group mb-md-0"> Select Role: </label>
+                                <label for="Tourist" class="radio-inline"><input type="radio" name="user_types_id" value= "Tourist" id="Tourist">Tourist</label>
+								 <label for="Business Owner" class="radio-inline"><input type="radio" name="user_types_id" value="Business Owner" id="Business Owner">Business Owner</label>
+                           
+                        </div>
+						</div>
+                        <div class="col-md-6">
+                           
+							<div class="form-group form-group-textarea mb-md-0">
+		
+                                <!-- Address input-->
+                                <textarea class="form-control" id="city_name" name="city_name" placeholder="Specify city *" data-sb-validations="required"></textarea>
+                                <div class="invalid-feedback" data-sb-feedback="city_name:required">Please enter city for a customized experience.</div>
+                            </div>
+                        </div>
+				
+<div class="col-md-6">
+						 <div class="form-group form-group-textarea mb-md-0">
+                                <input class="form-control" id="user_email" name="email" type="email" placeholder="Email: John@example.com *" data-sb-validations="required,email" />
+                                <div class="invalid-feedback" data-sb-feedback="user_email:required">An email is required.</div>
+                                <div class="invalid-feedback" data-sb-feedback="user_email:email">Email is not valid.</div>
+								<input class="form-control" id="user_password" name="pass" type="password" placeholder="Please enter password *" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" data-sb-validations="required" />
+                                <div class="invalid-feedback" data-sb-feedback="user_password:required">A password is required.</div>
+                            </div>
+                     </div>
+					
+                    <!-- Submit success message-->
+                    <!---->
+                    <!-- This is what your users will see when the form-->
+                    <!-- has successfully submitted-->
+                   <!-- <div class="d-none" id="submitSuccessMessage">
+                        <div class="text-center text-white mb-3">
+                            <div class="fw-bolder">Form submission successful!</div>
+                            To activate this form, sign up at
+                            <br />
+                            <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                        </div>
+                    </div> -->
+                    <!-- Submit error message-->
+                    <!---->
+                    <!-- This is what your users will see when there is-->
+                    <!-- an error submitting the form-->
+                    <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
+                    <!-- Submit Button-->
+                    <div class="text-center"><button class="btn btn-danger btn-xl text-uppercase enabled" id="submitButton" name="submit" value="submit" type="submit">Sign Up</button></div>
+                </form>
+            </div>
+        </section>
+        <!-- Contact-->
+        <section class="page-section" id="contact">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Glad to see you Back! </h2>
+                </div>
+                <!-- * * * * * * * * * * * * * * *-->
+                <!-- * * SB Forms Contact Form * *-->
+                <!-- * * * * * * * * * * * * * * *-->
+                <!-- This form is pre-integrated with SB Forms.-->
+                <!-- To make this form functional, sign up at-->
+                <!-- https://startbootstrap.com/solution/contact-forms-->
+                <!-- to get an API token!-->
+                <form id="logInForm" data-sb-form-api-token="API_TOKEN" action="logIn.php" >
+                    <div class="row align-items-stretch mb-5">
+                        <div class="col-md-6">
+						 <div class="form-group form-group-textarea mb-md-0">
+                                <input class="form-control" name="email" lid="email" type="email" placeholder="Email: John@example.com *" data-sb-validations="required,email" />
+                                <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
+                                <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
+								<input class="form-control" name="pass" id="pass" type="password" placeholder="Please enter password *" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" data-sb-validations="required" />
+                                <div class="invalid-feedback" data-sb-feedback="pass:required">A password is required.</div>
+                            </div>
+                     </div>
+              
+                    </div>
+                    <!-- Submit success message-->
+                    <!---->
+                    <!-- This is what your users will see when the form-->
+                    <!-- has successfully submitted-->
+                    <div class="d-none" id="submitSuccessMessage">
+                        <div class="text-center text-white mb-3">
+                            <div class="fw-bolder">Form submission successful!</div>
+                            To activate this form, sign up at
+                            <br />
+                            <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                        </div>
+                    </div>
+                    <!-- Submit error message-->
+                    <!---->
+                    <!-- This is what your users will see when there is-->
+                    <!-- an error submitting the form-->
+                    <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
+                    <!-- Submit Button-->
+                    <div class="text-center"><button class="btn btn-danger btn-xl text-uppercase enabled" id="logInButton" value="submit" name="logIn" type="submit">Sign In</button></div>
+                </form>
+            </div>
+        </section>
+       
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
