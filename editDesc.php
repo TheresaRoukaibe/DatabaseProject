356bf341431445eab1ef89423a4d664dad0ee54c
TheresaRoukaibe <102797454+TheresaRoukaibe@users.noreply.github.com>
2022-05-04 23:57:48 +0300

    change description

---
+++ editDesc.php
@@ -0,0 +1,34 @@
+<?php
+$pdo = new PDO ("mysql:host=localhost;dbname=touristhelper.db","root", "");
+include("connection.php");
+$att_name = $_GET['att_name'];
+$att_desc = $_GET['att_desc'];
+
+$db = mysqli_connect('localhost', 'root', '', 'touristhelper.db');
+$conn = new mysqli('localhost', 'root', '', 'touristhelper.db');
+
+if(empty($att_name) or empty($att_desc)){
+	echo '<script>alert("Please enter all values. Failed to update!");window.location.href = "Admin.php";</script>';
+}else{
+	if($conn->connect_error){
+	die($conn->connect_error);
+}else{
+	$sql = "SELECT * FROM tourist_attractions WHERE att_name=?"; 
+$result = $pdo->prepare($sql);
+$result->bindParam(1, $att_name);
+$result->execute();
+$place = $result->fetch();
+
+if(empty($place)){
+	echo '<script>alert("Could not find place. Failed to Update! Make sure of name");window.location.href = "Admin.php";</script>';
+}else{
+	$stmt = $conn->prepare("UPDATE tourist_attractions SET att_desc =? WHERE att_name= ?");
+	$stmt->bind_param("ss", $att_desc, $att_name);
+	$stmt->execute(); 
+		echo '<script>alert("Hours Updated. Refreshing...");window.location.href = "Admin.php";</script>';
+	$stmt->close();
+	$conn->close();
+	}
+}
+}
+?>
