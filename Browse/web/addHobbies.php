dd9b2d71d84f2e01576ef870160f4e39a77dadc6
TheresaRoukaibe <102797454+TheresaRoukaibe@users.noreply.github.com>
2022-05-05 13:46:02 +0300

    Changed restrictions to make hobbies unique

--- Browse/web/addHobbies.php
+++ Browse/web/addHobbies.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 
 $pdo = new PDO ("mysql:host=localhost;dbname=touristhelper.db","root", "");
 include("connection.php");
@@ -14,9 +15,7 @@ if($conn->connect_error){
 	die($conn->connect_error);
 }else{
 	
-	$stmt = $conn->prepare("INSERT INTO hobbies(hobby_name) VALUES(?)");
-	$stmt->bind_param("s", $hobby_name);
-	$stmt->execute(); 
+if($_SESSION['email'] == $email){	
 	
 $sql = "SELECT * FROM hobbies WHERE hobby_name=?"; 
 $result = $pdo->prepare($sql);
@@ -31,6 +30,7 @@ $result2->execute();
 $user = $result2->fetch();
 
 if($user){
+	if($hobby){
 $stmt2 = $conn->prepare("INSERT INTO user_enjoys(enjoys_user_id, enjoys_hobby_id) VALUES(?,?)");
 	$stmt2->bind_param("ii", $user['user_id'], $hobby['hobby_id']);
 	$stmt2->execute();
@@ -40,10 +40,27 @@ $stmt2 = $conn->prepare("INSERT INTO user_enjoys(enjoys_user_id, enjoys_hobby_id
 	
 	$stmt->close();
 	$conn->close();
+	}else{
+	$stmt = $conn->prepare("INSERT INTO hobbies(hobby_name) VALUES(?)");
+	$stmt->bind_param("s", $hobby_name);
+	$stmt->execute(); 
+	$sql3 = "SELECT * FROM hobbies WHERE hobby_name=?"; 
+$result3 = $pdo->prepare($sql3);
+$result3->bindParam(1, $hobby_name);
+$result3->execute();
+$hobby2 = $result3->fetch();
+	$stmt3 = $conn->prepare("INSERT INTO user_enjoys(enjoys_user_id, enjoys_hobby_id) VALUES(?,?)");
+	$stmt3->bind_param("ii", $user['user_id'], $hobby2['hobby_id']);
+	$stmt3->execute();
+	echo '<script>alert("Hobby Added. Click OK to refresh"); window.location.href = "browse.php";</script>';
+	}
 }else{
 	echo '<script>alert("User not found. Make sure of email"); window.location.href = "browse.php";</script>';
 }
+  }else{
+	  echo '<script>alert("Not your email. Make sure of email"); window.location.href = "browse.php";</script>';
   }
+}
 }
 
 
